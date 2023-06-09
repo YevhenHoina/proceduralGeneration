664be46289d6810a454bf6d081661fead6c332d4
YevhenHoina <60106209+YevhenHoina@users.noreply.github.com>
2023-06-09 15:11:56 +0300

    Ignore unUsed content from marketplace

--- Source/proceduralGeneration/proceduralGeneration.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "proceduralGeneration.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, proceduralGeneration, "proceduralGeneration" );
